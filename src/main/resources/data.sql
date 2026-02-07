68fc4e84bdbda5557d99189722fddcbc9a1e2a6f
Varshini K <23suca60@tcarts.in>
2026-02-07 17:45:58 +0530

    Add demo data for Render

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO donor (available, blood_group, city, name)
+VALUES (true, 'A+', 'Madurai', 'Ramya');
+
+INSERT INTO donor (available, blood_group, city, name)
+VALUES (true, 'B+', 'Coimbatore', 'Krish');
